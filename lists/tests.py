1781ef4fe5bc94a8df6d8eb74fd9aea08759267a
wstcpyt <wstcpyt@gmail.com>
2015-02-03 16:57:15 -0500

    change functional_test.py to test both itmes on the list

--- lists/tests.py
+++ lists/tests.py
@@ -25,5 +25,10 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'}
+        )
+        self.assertEqual(response.content.decode(),expected_html)
 
 
