9116174270249d13403ab64181f9e858da613f15
yutongpang <wstcpyt@gmail.com>
2015-02-03 10:58:05 -0500

    Front page HTML now generated from a templte

--- functional_test.py
+++ functional_test.py
@@ -19,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):  #1
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)  #5
         header_text = self.browser.find_element_by_tag_name("h1").text
-        self.assertIn('To-do',header_text)
+        self.assertIn('To-Do',header_text)
 
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -37,7 +37,8 @@ class NewVisitorTest(unittest.TestCase):  #1
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: But peacock feathers' for row in rows)
+            any(row.text == '1: But peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
