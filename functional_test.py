3bbfe45bbda7a0aa8871deb123deaba8bd636e6e
yutongpang <wstcpyt@gmail.com>
2015-02-03 23:08:03 -0500

    Refractor code solve duplicate

--- functional_test.py
+++ functional_test.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):  #1
     def tearDown(self):  #3
         self.browser.quit()
 
+    def check_for_row_in_list_table(self,row_test):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_test,[row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):  #4
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -45,13 +50,9 @@ class NewVisitorTest(unittest.TestCase):  #1
         imputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
