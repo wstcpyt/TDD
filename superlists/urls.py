95b15c7fd32be93e3e0b7ed1dbf81f18f43b2657
yutongpang <wstcpyt@gmail.com>
2015-02-04 23:09:32 -0500

    add url to add list

--- superlists/urls.py
+++ superlists/urls.py
@@ -3,8 +3,11 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-     url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',
+        name='view_list'
+    ),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
     #url(r'^admin/', include(admin.site.urls)),
 )
